4fe2a19f57869e070f229ca9961d193c98ccf63e
sh20raj <shaswatraj3@gmail.com>
2024-07-14 22:58:45 +0530

    chore: Add lazy loading for Google AdSense script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <head>
+      
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549"
+     crossorigin="anonymous"></Script>
+      </head>
       <body className={inter.className}>{children}</body>
       <Analytics/>
     </html>
