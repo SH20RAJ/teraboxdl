c866de6ea7924ddc03467a3e6f49f9d367927f3f
sh20raj <shaswatraj3@gmail.com>
2024-07-30 01:13:21 +0530

    seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,28 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "TeraDL - Terabox Downloader | 100% working | Download Terabox videos",
-  description: "Watch and download terabox Videos",
+  title: "Terabox.tech Player, Embed, Download Terabox videos",
+  description: "Watch, Embed and download terabox Videos | 100% working | Download Terabox videos with TeraDL",
+  keywords: "terabox, downloader, videos",
+  author: "Your Name",
+  robots: "index, follow",
+  ogTitle: "Terabox.tech Player, Embed, Download Terabox videos",
+  ogDescription: "Watch, Embed and download terabox Videos | 100% working | Download Terabox videos with TeraDL",
+  ogType: "website",
+  ogImage: "./og.png",
+  twitterTitle: "Terabox.tech Player, Embed, Download Terabox videos",
+  twitterDescription: "Watch, Embed and download terabox Videos | 100% working | Download Terabox videos with TeraDL",
+  twitterImage: "./og.png",
+  // Additional SEO tags
+  ogUrl: "https://terabox.tech",
+  ogSiteName: "Terabox.tech",
+  twitterCard: "summary_large_image",
+  twitterSite: "@microsoft",
 };
 
 let analytics = `
@@ -25,12 +40,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <div className="analytics">
           <div dangerouslySetInnerHTML={{__html: analytics}}></div>
         </div>
-      
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549"
-     crossorigin="anonymous"></Script>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549" crossorigin="anonymous"></Script>
       </head>
       <body className={inter.className}>{children}</body>
       <Analytics/>
