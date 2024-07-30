404ec80e0117e2c45aac67fbdfc3d0a2870a5221
sh20raj <shaswatraj3@gmail.com>
2024-07-30 09:33:28 +0530

    chore: Update Terabox Downloader component UI and metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,13 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Terabox.tech Player, Embed, Download Terabox videos",
-  description: "Watch, Embed and download terabox Videos | 100% working | Download Terabox videos with TeraDL",
+  description: "Watch, Embed and download Terabox Videos 🔥 100% working ✅ Download Terabox videos with TeraDL",
   keywords: ["terabox", "terabox direct download", "terabox downloader", "terabox online downloader", "terabox player", "terabox online player", "terabox links", "terabox link converter", "terabox direct link", "terabox direct videos", "terabox direct files", "terabox bypass", "terabox link bypass", "terabox video downloader", "terabox video download", "terabox link downloader", "terabox downloader online"],
   authors: [{ name: "Terabox.tech" }],
   robots: "index, follow",
   openGraph: {
     title: "Terabox.tech Player, Embed, Download Terabox videos",
-    description: "Watch, Embed and download terabox Videos | 100% working | Download Terabox videos with TeraDL",
+    description: "Watch, Embed and download Terabox Videos 🔥 100% working ✅ Download Terabox videos with TeraDL",
     type: "website",
     images: "./og.png",
     url: "https://terabox.tech",
@@ -21,7 +21,7 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Terabox.tech Player, Embed, Download Terabox videos",
-    description: "Watch, Embed and download terabox Videos | 100% working | Download Terabox videos with TeraDL",
+    description: "Watch, Embed and download Terabox Videos 🔥 100% working ✅ Download Terabox videos with TeraDL",
     images: "./og.png",
     site: "@microsoft",
   },
